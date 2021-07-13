90e20f1de706dd7fdd0f07e38f6a697f679de495
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-12 21:06:11 -0500

    added functions set and hello from coursera class on smart contracts

--- smartcontracts/greeter.sol
+++ smartcontracts/greeter.sol
@@ -2,10 +2,18 @@ pragma solidity ^0.4.0;
 
 contract Greeter {
     // define variable greeting of type string 
-    string public AaronA;
+    string public yourName;
 
     // this runs when the contract is executed
     function Greeter() public {
-        AaronA = "World";
+        yourName = "World";
+    }
+
+    function set(string name) public {
+        yourName = name;
+    }
+
+    function hello() constant returns (string) {
+        return yourName
     }
 }
