485c48d0395379a7a56c55e59b0e25b642fc5156
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-12 21:04:55 -0500

    added variable AaronAa and function Greeter from class contract

--- smartcontracts/greeter.sol
+++ smartcontracts/greeter.sol
@@ -1,5 +1,11 @@
 pragma solidity ^0.4.0;
 
 contract Greeter {
-    
+    // define variable greeting of type string 
+    string public AaronA;
+
+    // this runs when the contract is executed
+    function Greeter() public {
+        AaronA = "World";
+    }
 }
