8b48e27095d5c3053bd0509d7fade19961144f7c
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-12 20:05:31 -0500

    added exponentiantion from quiz in birdegree.sol

--- solidity/bitdegree.sol
+++ solidity/bitdegree.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.4.20;
 
 contract SpaceDoggos {
-    uint maxPlanetsPerSystem = 10;
-    uint minPlanetsPerSystem = 3;
+  uint maxPlanetsPerSystem = 10;
+  uint minPlanetsPerSystem = 3;
 
-    uint planetCodeDigits = 7;
-    uint systemCodeDigits = 7;
+  uint planetCodeDigits = 7;
+  uint systemCodeDigits = 7;
+
+  uint planetCodeModulus = 10 ** planetCodeDigits;
+  uint systemCodeModulus = 10 ** systemCodeDigits;
+
 }
